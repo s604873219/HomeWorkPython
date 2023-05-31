1a631314eaceaa0e369763c018c766c4e8d52b2c
Evgenii <s604873219@gmail.com>
2023-05-31 13:24:53 +0200

    Lesson4 HOmeWork

--- HomeWork.py
+++ HomeWork.py
@@ -1,26 +1,17 @@
-# Задача 26
+def summ(a, b):
+    print(f'a = {a}, b = {b}')
+    if b != 0:
+        return summ(a + 1, b - 1)
+    return a
+print(summ(2, 2))
 
-def step(n, m):
-    res = 1
-    for _ in range(m):
-        res *= n
-    return res
 
-n = int(input('Введите число'))
-m = int(input('Введите степень'))
-res = step(n, m)
-print(n, 'в', m, 'степени', ' = ', res)
+def step(A, B):
+    if B == 1:
+        return A
+    if ( B != 1):
+        return (A * step(A, B - 1))
+A = int(input('Введите число'))
+B = int(input('Введите степень'))
+print(A, 'в', B, 'степени', ' = ', step(A, B))
 
-# Задача 28
-
-def sum(a, b):
-    if b == 0:
-        return a
-    elif a == 0:
-        return b
-    else:
-        return sum(a + 1, b - 1)
-a = int(input('Введите первое число'))
-b = int(input('Введите второе число'))
-res = sum(a, b)
-print(a, '+', b, ' = ', res)
